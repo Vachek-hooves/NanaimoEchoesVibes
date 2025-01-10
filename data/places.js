ea90525d8972e67c422ea0b34cde9527b73d98b4
Vachek-hooves <vachek0602@gmail.com>
2025-01-10 13:25:09 +0200

    update data set

--- data/places.js
+++ data/places.js
@@ -3,30 +3,43 @@ export const PLACES = [
     id: '1',
     text: 'A beautiful marine park accessible by ferry, known for its hiking trails, beaches, and Indigenous cultural significance.',
     header: 'Newcastle Island',
+    coordinates: {latitude: 49.30692403386, longitude: -123.9318915096091},
+    image:'https://britishcolumbia.com/wp-content/uploads/2024/06/12e47e87-b7ab-4d08-b8a2-e53d1946eb27.jpeg'
+   
   },
   {
     id: '2',
     text: 'A popular spot for hiking, diving, and photography. It offers stunning ocean views, rocky cliffs, and serene coves.',
     header: 'Neck Point Park',
+    coordinates: {latitude: 49.23512525616737, longitude: -123.96566191770452},
+    image:'https://lh4.googleusercontent.com/proxy/lZe6lYK8Uf_Y7odPpw4OhmRdtlFDxY_tYQpEWSO78_vunt-UMyd2Ngk0m1_mtYCuZS6L9390-TElLnLCB6AsKj-7ccVD1cxr98LtP6VUajdCd6kh5jF2RVIsOu7Br6gmzjlg30iKSdID98McrF__rDbZzCg'
   },
   {
     id: '3',
     text: 'This scenic coastal park is ideal for bird watching, picnics, and sunset views. It features tidal pools and rugged coastal landscapes.',
     header: 'Pipers Lagoon Park',
+    coordinates: {latitude: 49.223020515039565, longitude: -123.94767714308583},
+    image:'https://discovervancouverisland.com/wp-content/uploads/2015/08/Pipers1-TimHartman.jpg'
   },
   {
     id: '4',
     text: 'Nanaimo’s oldest building, dating back to 1853. This wooden fort was built by the Hudson’s Bay Company and is a symbol of the city’s colonial history.',
     header: 'The Bastion',
+    coordinates: {latitude: 49.16721564301162, longitude: -123.93519171979646},
+    image:'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/be/e4/c5/the-bastion.jpg?w=1200&h=-1&s=1'
   },
   {
     id: '5',
     text: 'A peaceful escape with lakes, forested trails, and waterfalls. A great location for swimming and nature walks.',
     header: 'Colliery Dam Park',
+    coordinates: {latitude: 49.1515476046737, longitude: -123.96049792580486},
+    image:'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/c0/94/bc/colliery-dam-park.jpg?w=1200&h=-1&s=1'
   },
   {
     id: '6',
     text: 'Learn about the city’s history, including exhibits on Indigenous culture, coal mining, and the famous bathtub races.',
     header: 'Nanaimo Museum',
+    coordinates: {latitude: 49.165136986717165, longitude: -123.93607946397375},
+    image:'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/bd/dc/49/nanaimo-museum.jpg?w=1200&h=-1&s=1'
   },
 ];
